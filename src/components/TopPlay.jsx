d0427a15b09102229dac6db0e2748713b886b6bb
Jose0112Luis <jose0112luis@hotmail.com>
2022-09-29 13:29:47 -0500

    Swiper / Deslizador de la sección de Top Artists.

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -43,6 +43,7 @@ const TopPlay = () => {
 
   return (
     <div ref={ divRef } className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
+      
       <div className='w-full flex flex-col'>
         {/* Header Top Charts */}
         <div className='flex flex-row justify-between items-center'>
@@ -66,7 +67,41 @@ const TopPlay = () => {
         </div>
       </div>
 
-      
+      <div className='w-full flex flex-col mt-8'>
+        {/* Header Top Artists */}
+        <div className='flex flex-row justify-between items-center'>
+          <h2 className='text-white font-bold text-2xl'>Top Artists</h2>
+          <Link to='/top-artists'>
+            <p className='text-gray-300 text-base cursor-pointer'>See more</p>
+          </Link>
+        </div>
+
+        {/* Swiper de Top Artists */}
+        <Swiper 
+          slidesPerView='auto'
+          spaceBetween={ 15 }
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={ [FreeMode] }
+          className='mt-4'
+        >
+          {
+            topPlays?.map((song, i) => (
+              <SwiperSlide
+                key={ song?.key }
+                style={{ width: '25%', height: 'auto' }}
+                className='shadow-lg rounded-full animate-slideright'
+              >
+                <Link to={`/artists/${song?.artists[0].adamid}`}>
+                  <img src={song?.images.background} alt='name' className='rounded-full w-full object-cover' />
+                </Link>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+
     </div>
   );
 }
